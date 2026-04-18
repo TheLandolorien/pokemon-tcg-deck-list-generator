ddae0f3e62c910ec59cf96358de4e2e70c48739d
Lando Toothman <25360446+TheLandolorien@users.noreply.github.com>
2026-04-18 13:32:36 -0400

    feat: Generate deck list template paths

--- src/pokemon_tcg_deck_list_generator/cli.py
+++ src/pokemon_tcg_deck_list_generator/cli.py
@@ -5,7 +5,7 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="ptcg-generate-decklist", description="Generate PDF Pokemon TCG deck lists from PTCGL exports."
     )
-    parser.add_argument("-f", "--format", help="Standard format abbreviation (e.g., ASC)")
+    parser.add_argument("-f", "--format", help="Standard format abbreviation (e.g., ASC)", default="POR")
     parser.add_argument("-d", "--deck-export", help="Path to Deck export", required=True)
 
     return parser.parse_args()
--- src/pokemon_tcg_deck_list_generator/main.py
+++ src/pokemon_tcg_deck_list_generator/main.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 import pokemon_tcg_deck_list_generator.cli as cli
 
 
 def run() -> None:
     args = cli.parse_args()
 
-    print(args.format, args.deck_export)
+    deck_list_template_path = Path("assets") / f"{args.format}.pdf"
+
+    print(deck_list_template_path, args.deck_export)
